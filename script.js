fff5b8047cb6c1abe13cd0593035968888d7e84a
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-02-22 20:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+function manipulateData(inputArray) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(inputArray);
+    }, 3000);
+  })
+    .then((array) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const evenNumbers = array.filter((num) => num % 2 === 0);
+          document.getElementById("output").innerText = evenNumbers.join(", ");
+          resolve(evenNumbers);
+        }, 1000);
+      });
+    })
+    .then((evenNumbers) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const doubledNumbers = evenNumbers.map((num) => num * 2);
+          document.getElementById("output").innerText =
+            doubledNumbers.join(", ");
+          resolve(doubledNumbers);
+        }, 2000);
+      });
+    });
+}
+
+const inputArray = [1, 2, 3, 4];
+manipulateData(inputArray);
